f4b63783132e501920b7cd358b6aa5e8f5ad98a6
root <shx26@pitt.edu>
2020-04-06 10:54:00 -0400

    modified test script for recon-all

--- ADNI_preprocessing/test_freesurfer_build.sh
+++ ADNI_preprocessing/test_freesurfer_build.sh
@@ -3,6 +3,15 @@
 #usage          :source build_freesurfer_SOURCE.sh
 #===============================================================
 
+#test mri-convert
+echo "testing mri convert"
 sudo cp $FREESURFER_HOME/subjects/sample-001.mgz .
 mri_convert sample-001.mgz sample-001.nii.gz
+
+#test recon-all
+echo "testing recon-all"
+sudo python3 hdr_img2nii.py -p test_images
+rm -rf $SUBJECTS_DIR/recon_test
+#sudo mkdir $SUBJECTS_DIR/recon_test
+recon-all -i test_images/002_S_0619/ADNI_002_S_0619_MR_MP-RAGE__br_raw_20060601215738863_1_S15147_I16392.nii -subject recon_test
 
