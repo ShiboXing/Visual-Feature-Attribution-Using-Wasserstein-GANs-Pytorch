e8feb6d8998f3cfabded25c7aff692eb7df70c30
user.name <shx26@pitt.edu>
2020-04-29 15:16:55 +0800

    cleaned autorecon shell scripts, added gitignore

--- ADNI_preprocessing/autorecon1_shell.sh
+++ ADNI_preprocessing/autorecon1_shell.sh
@@ -33,9 +33,9 @@ mkdir fetch_preprocessed/skullstrip
 cp /usr/local/freesurfer/subjects/recon_test/mri/brainmask.mgz fetch_preprocessed/skullstrip
 
 #perform co-registration with mni-space
-mkdir fetched_preprocessed/mni_registered
-mri_robust_register --mov fetch_preprocessed/skullstrip/brainmask.mgz --dst test_images/mni_space/mni_icbm152_t1_tal_nlin_asym_09c_mask.nii --lta fetch_preprocessed/mni_registered/reg.lta
-mri_convert fetch_preprocessed/skullstrip/brainmask.mgz fetch_preprocessed/skullstrip/mni_reg.nii --apply_transform fetch_preprocessed/mni_registered/reg.lta
+mkdir fetch_preprocessed/mni_registered
+mri_robust_register --mov fetch_preprocessed/skullstrip/brainmask.mgz --dst test_images/mni_space/mni_icbm152_t1_tal_nlin_asym_09c_mask.nii --lta fetch_preprocessed/mni_registered/reg.lta --satit 
+mri_convert fetch_preprocessed/skullstrip/brainmask.mgz fetch_preprocessed/mni_registered/mni_reg.nii --apply_transform fetch_preprocessed/mni_registered/reg.lta
 
 #convert all results to nifti
 sh mgz2nii.sh
--- ADNI_preprocessing/mni_register.sh
+++
@@ -1,4 +0,0 @@
-#perform co-registration with mni-space
-mkdir fetch_preprocessed/mni_registered
-mri_robust_register --mov fetch_preprocessed/skullstrip/brainmask.mgz --dst test_images/mni_space/mni_icbm152_t1_tal_nlin_asym_09c_mask.nii --lta fetch_preprocessed/mni_registered/reg.lta --satit
-mri_convert fetch_preprocessed/skullstrip/brainmask.mgz fetch_preprocessed/mni_registered/mni_reg.nii --apply_transform fetch_preprocessed/mni_registered/reg.lta
